4d2fe0b278c7dd94a27f94b46674cee88e2ba32f
Tom Mayo <t.mayo@ed.ac.uk>
2017-01-25 21:52:31 +0000

    got rid of the parallel problems
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/M3D@126158 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_lite_fns.R
+++ tests/testthat/test_lite_fns.R
@@ -1,5 +1,4 @@
 library('M3D')
-library('parallel')
 context('lite functions')
 
 test_that('M3D_Wrapper_lite works properly', {
@@ -33,23 +32,3 @@ test_that('pvals_lite works properly', {
                            smaller=FALSE,comparison='allReps',method='empirical',
                            closePara=0.005),PDemo, tolerance=1.0e-4)
 })
-
-test_that('M3D_Para_lite works properly', {
-    library(parallel)
-    library(BiSeq)
-    library('GenomicRanges')
-    data(rrbsDemo)
-    data(CpGsDemo)
-    data(MMDlistDemo)
-    CpGs1 <- CpGsDemo[1:100]
-    overlaps1 <- findOverlaps(CpGs1,rrbsDemo)
-    overlaps2 <- findOverlaps(CpGsDemo,rrbsDemo)
-    M3D_stat_lite1 <- M3D_Para_lite(rrbsDemo,CpGs1,overlaps1,num.cores=4)
-    M3D_stat_lite2 <- M3D_Para_lite(rrbsDemo,CpGsDemo,overlaps2,num.cores=4)
-    stat <- MMDlistDemo[[1]]-MMDlistDemo[[2]]
-    res <- cbind(rowMeans(stat[,2:5],na.rm=TRUE),stat[,1],stat[,6])
-    colnames(res) <- c('MeanBetween','H1-hESC1  vs  H1-hESC2','K562-1  vs  K562-2')
-    expect_equal(res[1:100,],M3D_stat_lite1,tolerance=1e-4)
-    expect_equal(res,M3D_stat_lite2,tolerance=1e-4)
-})
-
